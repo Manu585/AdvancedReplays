52915a7a81f19fb67947a97f92308b1cbf8ef1eb
Manu585 <manuelgogg2@gmail.com>
2026-02-08 17:58:46 +0100

    Got rid of falsely placed files.

--- advancedreplays-core/src/main/java/com/github/manu585/advancedreplays/core/factory/ActorFactory.java
+++
@@ -1,7 +0,0 @@
-package com.github.manu585.advancedreplays.core.factory;
-
-public class ActorFactory {
-
-
-
-}
--- advancedreplays-core/src/main/java/com/github/manu585/advancedreplays/core/nms/ReplayPlayer.java
+++
@@ -1,4 +0,0 @@
-package com.github.manu585.advancedreplays.core.nms;
-
-public class ReplayPlayer {
-}
